fedb7fb9ffe99e5c50f95c148938765f05306d9b
Christian Lindell <christian_lindell@yahoo.com>
2024-09-15 22:09:48 +0200

    Fixat "api-url"-tester för KI:s statistik och prognosdatas

--- tests/testthat/test-fetchpxw.R
+++ tests/testthat/test-fetchpxw.R
@@ -7,3 +7,34 @@ test_that("pxw_skapa_api_url() SCB fungerar", {
     "https://api.scb.se/OV0104/v1/doris/sv/ssd/START/AM/AM0210/AM0210A/ArbStatusM")
 })
 
+
+# test_that("pxw_skapa_api_url() Tillvaxtanalys fungerar", {
+#         expect_equal(pxw_create_api_url("https://statistik.tillvaxtanalys.se/PxWeb/pxweb/sv/Tillv%C3%A4xtanalys%20statistikdatabas/Tillv%C3%A4xtanalys%20statistikdatabas__Konkurser%20och%20offentliga%20ackord/konk_ack_ar_1996.px/"),
+#                      "")
+#
+#     })
+
+test_that("pxw_skapa_api_url() FoHM fungerar", {
+        expect_equal(pxw_create_api_url("http://fohm-app.folkhalsomyndigheten.se/Folkhalsodata/pxweb/sv/A_Folkhalsodata/A_Folkhalsodata__Z_ovrigdata__Arbete/Arbetsloshet_LanUTBNIVA.px/"),
+                     "http://fohm-app.folkhalsomyndigheten.se/Folkhalsodata/api/v1/sv/A_Folkhalsodata/Z_ovrigdata/Arbete/Arbetsloshet_LanUTBNIVA.px")
+
+    })
+
+test_that("pxw_skapa_api_url() Jordbruksverket fungerar", {
+    expect_equal(pxw_create_api_url("https://statistik.sjv.se/PXWeb/pxweb/sv/Jordbruksverkets%20statistikdatabas/Jordbruksverkets%20statistikdatabas__Sysselsattning/JO1101Q01.px/?rxid=5adf4929-f548-4f27-9bc9-78e127837625"),
+                 "https://statistik.sjv.se/PXWeb/api/v1/sv/Jordbruksverkets statistikdatabas/Sysselsattning/JO1101Q01.px")
+
+})
+
+test_that("pxw_skapa_api_url() Konjinst statistikdatabas fungerar", {
+    expect_equal(pxw_create_api_url("https://statistik.konj.se/PxWeb/pxweb/sv/KonjBar/KonjBar__ftgregional/Bartotreg.px/"),
+                 "https://statistik.konj.se:443/PxWeb/api/v1/sv/KonjBar/ftgregional/Bartotreg.px")
+
+})
+
+test_that("pxw_skapa_api_url() Konjinst prognosdatabas fungerar", {
+    expect_equal(pxw_create_api_url("https://prognos.konj.se/PxWeb/pxweb/sv/SenastePrognosen/SenastePrognosen__f03_internationellbnpochkonsumentpriser/F0301.px/"),
+                 "https://prognos.konj.se:443/PxWeb/api/v1/sv/SenastePrognosen/f03_internationellbnpochkonsumentpriser/F0301.px")
+
+})
+
