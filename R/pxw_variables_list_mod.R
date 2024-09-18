c5e8c4c7475240a7a6c5eda6c6d42aff71b03844
Christian Lindell <christian_lindell@yahoo.com>
2024-09-18 15:24:48 +0200

    Gör om variabellistan så att den har samma uppbyggnad oavsett om pxwebtabellen är felkonfigurerad (löpnummer!) eller inte.

--- R/pxw_variables_list_mod.R
+++ R/pxw_variables_list_mod.R
@@ -20,23 +20,69 @@ pxw_variables_list_mod <- function(url_str, lopnr_in_values = FALSE) {
                "m\u00E5nad", "Manad", "manad", "Kvartal","kvartal", "year",
                "Month", "month", "Period", "period", "Time", "time")
 
+    korrekt_konfig_sajter <- paste(c("scb", "folkhalsomyndigheten", "konj.se"), collapse = "|")
+
+    felkonfig_sajter <- paste(c("tillvaxtanalys", "sjv.se"), collapse = "|")
+
+    # Funktion för att om första ordet kan vara en kod och antal ord
+    # En kod definieras av att den inte enbart består av bokstäver utan av enbart
+    # siffror, en blandning av siffror och bokstäver. Den får även innehålla ".+-_/".
+    # För att första ordet ska kunna vara en kod måste den följas av minst ett ord till
+    check_first_word <- function(text) {
+        words <- str_split(text, "\\s+")[[1]]
+        if (length(words) < 2) {
+            return(FALSE)
+        }
+        first_word <- words[1]
+        return(str_detect(first_word, "^[0-9a-zA-Z\u00e5\u00e4\u00f6\u00c5\u00c4\u00d6\\-\\+_/]+$") && str_detect(first_word, "[0-9]"))
+    }
+
     dfvar <- pxw_variables_list(url_str) %>%
-        mutate(values_org = values)
+        mutate(values_org = values,
+               valueText_org = valueText)
+
+
+
 
     #  Om servern är rätt konfigurerad
-    if (str_detect(url_str, "scb|folkhalsomyndigheten|konj.se")) {
-        return(dfvar)
-        # Om servern är felkonfigurerad och använder löpnr i values
-    } else if (str_detect(url_str, "tillvaxtanalys|sjv.se") | isTRUE(lopnr_in_values)){
+    if (str_detect(url_str, korrekt_konfig_sajter)) {
         dfvar <- dfvar %>%
-            mutate(time = if_else(text %in% tider,
-                                  TRUE, FALSE)) %>%
-            mutate(values = if_else(str_detect(valueText, "\\b(?=\\w*[0-9])[\\w.+-]+\\b\\s"),
-                                    str_extract(valueText, "\\b(?=\\w*[0-9])[\\w.+-]+\\b\\s"),
+            mutate(is_lopnr = FALSE)
+        return(dfvar)
+
+
+        # Om servern är felkonfigurerad och använder löpnr i values
+        # } else if (str_detect(url_str, felkonfig_sajter) | isTRUE(lopnr_in_values)){
+    } else {
+        dfvar <- dfvar %>%
+            group_by(code) %>%
+            mutate(values = as.numeric((values))) %>%
+            mutate(is_lopnr = if_else(between((max(values, na.rm = TRUE) / n()), 0.85, 1.15),
+                                      true = TRUE,
+                                      false =  FALSE)) %>%
+            ungroup() %>%
+            group_by(code) %>%
+            mutate(values = as.numeric((values))) %>%
+            mutate(ar_kod = sapply(valueText, check_first_word)) %>%
+            ungroup() %>%
+            mutate(values = if_else(ar_kod,
+                                    str_extract(valueText, "(?<!\\S)-?\\b(?=\\w*[0-9])[-\\w.+]*\\b-?"),
                                     valueText),
-                   values = str_trim(values),
-                   valueText = str_remove(valueText, "\\b(?=\\w*[0-9])[\\w.+-]+\\b\\s"))
+                   valueText = str_remove(valueText, "(?<!\\S)-?\\b(?=\\w*[0-9])[-\\w.+]*\\b-?")) %>%
+            select(-ar_kod) %>%
+            mutate(valueText = str_trim(valueText))
     }
+
+    # Om värdena i valuText inte är unika efter att den inledande koddelen raderats så
+    # ersätt dem med orginalvärdena som finns sparade i kolumnen valueText_org
+    dfvar <- dfvar %>%
+        group_by(code) %>%
+        mutate(valueText = case_when(
+            n_distinct(valueText) == n() ~ valueText,
+            TRUE ~ valueText_org
+        )) %>%
+        ungroup() %>%
+        mutate(valueText = str_trim(valueText))
 
     return(dfvar)
 }
