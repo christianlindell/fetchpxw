34b56b9a809a66ad52ca8fc45bfc7744ca9cd002
Christian Lindell <christian_lindell@yahoo.com>
2024-09-20 13:26:37 +0200

    Definierat globala variabler för att slippa notes vid R CMD BUILD

---
+++ R/globals.R
@@ -0,0 +1,5 @@
+utils::globalVariables(c(
+    "code", "values", "values_org", "parameter_namn", "text",
+    "valueText_org", "valueText", "reg_koder", "region_kod",
+    "elimination", "ar_kod"
+))
